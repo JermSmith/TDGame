66fbaae43483b8624a1d9e80b1df47e19d54faae
JermSmith <jerm_smith@hotmail.com>
2018-07-20 02:51:30 -0500

    Util

--- Math.h
+++ Math.h
@@ -14,3 +14,9 @@ T toDegrees(T radians)
 {
 	return (radians * 180) / PI;
 }
+
+template <typename T>
+T distanceBetweenPoints(T x1, T y1, T x2, T y2)
+{
+	return (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+}
